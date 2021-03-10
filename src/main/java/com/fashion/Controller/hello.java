f51881281b2a581e7ff4cac3969806e74d31116e
nhatnguyen727 <nhatnguyen727@gmail.com>
2021-03-10 22:03:18 +0700

    api 2

---
+++ src/main/java/com/fashion/Controller/hello.java
@@ -0,0 +1,12 @@
+package com.fashion.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hello {
+	@GetMapping("/")
+	public String hello() {
+		return "hello";
+	}
+}
